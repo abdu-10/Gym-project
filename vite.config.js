2c4ff14830d5a04ce974c42df864fa7a125ab971
Abdu <abdulrahmanammar17@gmail.com>
2025-11-29 01:21:41 +0300

    feat: add PWA support with VitePWA configuration in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,42 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(), 
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      
+      devOptions: {
+        enabled: true // <--- THIS FORCES THE PWA TO WORK IN DEV
+      },
+     
+
+      manifest: {
+        name: 'FitElite Access',
+        short_name: 'FitElite',
+        description: 'Premium Gym Access Control',
+        theme_color: '#000000',
+        background_color: '#000000',
+        display: 'standalone',
+        orientation: 'portrait',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
 })
